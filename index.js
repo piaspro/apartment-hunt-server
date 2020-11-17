5c78c36e854410565b87947d8fabfed190d9e8fe
piaspro <golamrahman09@gmail.com>
2020-11-17 21:06:39 +0600

    post done

--- index.js
+++ index.js
@@ -4,15 +4,44 @@ const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
-
+// pass: uJHArSG7Zz6ruhs;
 // use app
 const app = express()
 
 app.use(bodyParser.json());
 app.use(cors())
 
-app.get('/', (req,res) => {
-    res.send("hello world")
-})
+const uri = "mongodb+srv://apartmentUser:uJHArSG7Zz6ruhs@cluster0.z0xlc.mongodb.net/apartmentDb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+client.connect(err => {
+  const apartmentBookingCollection = client.db("apartmentDb").collection("apartmentBooking");
+  console.log("connected")
+  // Add Request booking
+  app.post('/addRequest', (req, res) => {
+    const userInfo = req.body
+    console.log(userInfo)
+    apartmentBookingCollection.insertOne(userInfo)
+      .then(result => {
+        console.log(result);
+        res.send(result.insertedCount > 0)
+      })
+
+    // Show Services in the home page
+    app.get('/getBookings', (req, res) => {
+      apartmentBookingCollection.find({})
+        .toArray((err, documents) => {
+          res.send(documents)
+        })
+    });
+    
+  });
+
+  app.get('/', (req, res) => {
+    res.send("connected")
+  });
+});
+
 
 app.listen(process.env.PORT || 5000);
